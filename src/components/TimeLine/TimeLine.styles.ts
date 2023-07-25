d8a2f30e9180e0834a9b58bdcecb3e9045eee5ec
Albert Conceição <developer.albert@outlook.com>
2023-07-24 22:52:16 -0300

    feat: timeline finished

--- src/components/TimeLine/TimeLine.styles.ts
+++ src/components/TimeLine/TimeLine.styles.ts
@@ -5,6 +5,35 @@ min-height: 1213px;
 background: #fff;
 `;
 export const Content = styled.div`
+
+h1 {
+    max-width: 362px;
+    margin: 0 auto;
+    text-align: center;
+    color: #2c2c2c;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 111.5%;
+    margin-bottom: 62px;
+
+    strong {
+        color: #e0623f;
+    }
+    @media (max-width: 768px) {
+        font-size: 20px;
+        max-width: 207px;
+    }
+}
+.circle {
+    width: 18px;
+    background: #806D61;
+    height: 18px;
+    margin: 0 auto;
+    border-radius: 10px;
+
+   
+}
 .line {
     margin: 0 auto;
     height: 821px;
@@ -15,8 +44,19 @@ export const Content = styled.div`
     justify-content: space-evenly;
     align-items: center;
     gap: 20px;
+
 }
-.time-content {
+@media (max-width: 768px) {
+    .circle, .line {
+        margin: unset;
+        margin-right: auto;
+    }
+    .line {
+        margin-left: 8px;
+    }
+}
+
+.time-content-web {
     position: relative;
     top: -737px;
     display: flex;
@@ -41,6 +81,7 @@ export const Content = styled.div`
         display: flex;
         align-items: center;
         gap: 20px;
+
     }
     .left {
         background: url('./time-left.png') no-repeat left;
@@ -49,5 +90,135 @@ export const Content = styled.div`
         background: url('./time-right.png') no-repeat right;
         padding: 35px 30px 30px 88px;
     }
+    @media (max-width: 1024px) {
+            display: none ;
+        
+        .left, .right {
+            min-width:372px;
+            max-width:372px;
+        
+            font-size: 16px;
+        }
+        .right {
+            padding: 35px 30px 30px 69px;
+        }
+        .left {
+            padding: 25px 59px 25px 46px;
+        }
+        .time-content-web {
+            top: -751px;
+        }
+    }
+    @media (min-width: 1024px) {
+        display: flex;
+    }
+}
+.time-content-mobile {
+    position: relative;
+    top: -737px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    max-width: 1100px;
+    margin: 0 auto;
+    margin-left: 40px;
+    gap: 48px;
+
+    div {
+        
+        min-width:279px;
+        max-width:279px;
+        max-height:82px;
+    }
+    .right {
+        max-width:279px;
+        max-height:82px;
+        padding: 25px 21px;
+        color: #2c2c2c;
+        font-size: 16px;
+        display: flex;
+        align-items: center;
+        gap: 20px;
+
+    }
+    .right {
+        background: url('./time-right-mb.png') no-repeat right;
+    }
+    @media (min-width: 1024px) {
+        display: none;
+    }
+    @media (min-width: 768px) and (max-width: 1024px){
+        align-items: flex-end;
+    }
+    }
+}
+
+@media (max-width: 1024px) {
+    .line img {
+        max-width: 51px;
+        max-height: 51px;
+    }
+    .time-content-web {
+        display: none;
+        display: none ;
+        
+        .left, .right {
+            min-width:279px !important;
+            max-width:279px !important;
+        
+            font-size: 16px;
+        }
+        .right {
+            padding: 35px 30px 30px 69px;
+        }
+        .left {
+            padding: 25px 59px 25px 46px;
+        }
+        .time-content-web {
+            top: -751px;
+        }
+    }
+    
+    
+}
+@media (min-width: 1024px) {
+    .time-content-web {
+    position: relative;
+    top: -737px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    max-width: 1100px;
+    margin: 0 auto;
+    gap: 20px;
+    div {
+        &:nth-child(even) {
+            align-self: flex-end;
+        }
+        max-width: 405px;
+    }
+
+    .left, .right {
+        min-width: 405px;
+        min-height: 110px;
+        padding: 25px 59px 25px 59px;
+        color: #2c2c2c;
+        font-size: 18px;
+        display: flex;
+        align-items: center;
+        gap: 20px;
+
+    }
+    .left {
+        background: url('./time-left.png') no-repeat left;
+    }
+    .right {
+        background: url('./time-right.png') no-repeat right;
+        padding: 35px 30px 30px 88px;
+    }
+    
+}
+    
+    
 }
 `;
--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -4,9 +4,11 @@ export function TimeLine() {
   return (
     <Container>
       <Content className='container'>
-        <h1>TimeLine</h1>
+        <h1>É assim que funciona o <strong>nosso atendimento</strong></h1>
 
         <section>
+          <div className='circle'></div>
+          
           <div className='line'>
             <div><img src="./number-1.png" /></div>
             <div><img src="./number-2.png" alt="" /></div>
@@ -15,7 +17,8 @@ export function TimeLine() {
             <div><img src="./number-5.png" alt="" /></div>
             <div><img src="./number-6.png" alt="" /></div>
           </div>
-          <div className='time-content'>
+          <div className='circle'></div>
+          <div className='time-content-web'>
             <div className="left" >
               <img src="./help-whatsapp.svg" alt="" /> <p>Você conta para mim a sua situação via <strong>Whatsapp</strong></p>
             </div>
@@ -25,6 +28,22 @@ export function TimeLine() {
             <div className="left" ><img src="./help-pen.svg" alt="" /><p>Assina a <strong>procuração</strong></p> </div>
             <div className="right"><p>Dou entrada no <strong>processo</strong> </p><img src="./help-hammer.svg" alt="" /></div>
 
+          </div>
+          <div className='time-content-mobile'>
+            <div className="right" >
+              <img src="./help-whatsapp.svg" alt="" /> <p>Você conta para mim a sua situação via <strong>Whatsapp</strong></p>
+            </div>
+            <div className="right">
+              <img src="./help-partners.svg" alt="" />
+              <p>Marcamos uma <strong>conversa presencial ou online</strong></p></div>
+            <div className="right" ><img src="./help-folder.svg" alt="" /> <p>Separe os <strong>documentos </strong>que comprovam seus direitos e sua <strong>Carteira de Trabalho</strong></p></div>
+            <div className="right">
+              <img src="./help-lock.svg" alt="" />
+              <p>Me conte <strong>todos os detalhes</strong> do caso </p> </div>
+            <div className="right" ><img src="./help-pen.svg" alt="" /><p>Assina a <strong>procuração</strong></p> </div>
+            <div className="right">
+              <img src="./help-hammer.svg" alt="" />
+              <p>Dou entrada no <strong>processo</strong> </p></div>
           </div>
         </section>
       </Content>
